2c3c13c1ab194b2aa583e6bab68090b3c2bbed40
Methara Sansiluni <metharasansiluni2003@gmail.com>
2025-02-08 00:46:29 +0530

    Migration  and controller

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,12 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use  Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
+    use HasFactory;
+
     protected $table ='categories';
 
     protected $fillable = [
         'category_name',
     ];
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
 
@@ -16,6 +17,7 @@ Route::get('/', function () {
 });
 
 Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
